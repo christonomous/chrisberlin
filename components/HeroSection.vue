d577f687aa2db4d8d3064c9f0c6993df5221e69a
christonomous <chris@brane.media>
2026-01-22 21:55:09 +0100

    update

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -3,10 +3,10 @@
     <div class="container mx-auto max-w-6xl px-4 md:pt-12 md:pt-36 grid md:grid-cols-[1.1fr,0.9fr] gap-10 items-center">
       <div>
         <h1 class="text-4xl md:text-6xl leading-tight font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-primary via-secondary to-accent animate-gradient">
-          Smart Trading
+          Smart MoneyTrading
         </h1>
         <p class="opacity-80 text-lg md:text-xl max-w-2xl">
-          Hi, I'm Chris and <b>I am building an onchain trading bot</b>.
+          Hi, I'm Chris and <b>I am building an onchain orderflow trading bot</b>.
         </p>
         <div class="mt-8 flex flex-wrap gap-3">
           <a href="#service" class="btn bg-gradient-to-r from-primary via-secondary to-accent animate-gradient text-base-100 border-0">Service</a>
